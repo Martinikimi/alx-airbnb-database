2d90677186b84d64db38ae79847a2a80776925e1
Martin Kimani <martinikimi7@gmail.com>
2025-09-04 22:44:25 +0300

    Update database_index.sql

--- database-adv-script/database-adv-script/database_index.sql
+++ database-adv-script/database-adv-script/database_index.sql
@@ -1,1 +1,14 @@
 
+-- 📌 Indexes for Airbnb Clone Database
+
+-- Index on user_id in Bookings table to speed up joins with Users
+CREATE INDEX idx_bookings_user_id ON bookings(user_id);
+
+-- Index on property_id in Bookings table to speed up joins with Properties
+CREATE INDEX idx_bookings_property_id ON bookings(property_id);
+
+-- Index on location in Properties table to optimize location-based searches
+CREATE INDEX idx_properties_location ON properties(location);
+
+-- Index on email in Users table to speed up user lookups
+CREATE INDEX idx_users_email ON users(email);
